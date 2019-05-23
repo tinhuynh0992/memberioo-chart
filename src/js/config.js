cf727936c5e88489a6176c7cd2745cc2af5aa3da
unknown <tinvuong51003405@gmail.com>
2019-05-23 23:19:24 +0700

    Allow dynamic change legend color

---
+++ src/js/config.js
@@ -0,0 +1,26 @@
+const TYPES_CONFIG = {
+  "Visitor": {
+    series: {
+      stroke: "#1abc9c",
+      fill: "#1abc9c"
+    }
+  },
+  "Pageviews": {
+    series: {
+      stroke: "#2ecc71",
+      fill: "#2ecc71"
+    }
+  },
+  "Leads": {
+    series: {
+      stroke: "#f1c40f",
+      fill: "#f1c40f"
+    }
+  },
+  "Sales": {
+    series: {
+      stroke: "#3498db",
+      fill: "#3498db"
+    }
+  }
+}
